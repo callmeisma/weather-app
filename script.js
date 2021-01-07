1024b0cf0d8074dfb3657c3b9a0f680a3f77b91d
callmeisma <ismael.fdez87@gmail.com>
2021-01-06 19:23:42 -0500

    initial working app

---
+++ script.js
@@ -0,0 +1,49 @@
+// get location from user
+function getInput() {
+    const userInput = document.getElementById('userInput').value;
+    clearDisplay()
+    getWeather(userInput,'imperial');
+}
+
+// fetch weather from user location
+async function getWeather(location, units) {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?&appid=50b1eebb44635563594202ca273ba117&q=${location}&units=${units}`, {mode: 'cors'})
+    if (response.status === 400) {
+        alert('Please input desired city weather');
+    } else {
+        const weatherData = await response.json();
+        const cleanData = processData(weatherData);
+        displayData(cleanData);
+    }   
+}
+
+function processData(weatherData) {
+    const cleanData = {
+    city: weatherData.name,
+    sky: weatherData.weather[0].main,
+    temperature: weatherData.main.temp,
+    feels: weatherData.main.feels_like,
+    humidity: weatherData.main.humidity,
+    }
+
+    return cleanData
+}
+
+function displayData(cleanData) {
+    const display = document.getElementById('display');
+
+    for (const [key, value] of Object.entries(cleanData)) {
+      const info = (`${key} = ${value}`);
+      const para = document.createElement('p');
+      para.innerText = info;
+      display.appendChild(para);
+    }
+}
+
+function clearDisplay() {
+    const display = document.getElementById('display');
+
+    while (display.firstChild) {
+        display.removeChild(display.lastChild);
+      }
+}
