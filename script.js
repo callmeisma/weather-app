b4af3f44f1550238df0361a3863a007eb0d4d971
callmeisma <ismael.fdez87@gmail.com>
2022-01-05 00:23:12 -0500

    frontend improvements

--- script.js
+++ script.js
@@ -1,51 +1,94 @@
-const apiKey = '';
+const apiKey = "50b1eebb44635563594202ca273ba117";
+let unit = "imperial";
 
 // get location from user
 function getInput() {
-    const userInput = document.getElementById('userInput').value;
-    clearDisplay()
-    getWeather(userInput,'imperial');
+  const userInput = document.getElementById("userInput").value;
+  getWeather(userInput, unit);
 }
 
 // fetch weather from user location
 async function getWeather(location, units) {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?&appid=${apiKey}&q=${location}&units=${units}`, {mode: 'cors'})
-    if (response.status === 400) {
-        alert('Please input desired city weather');
-    } else {
-        const weatherData = await response.json();
-        const cleanData = processData(weatherData);
-        displayData(cleanData);
-    }   
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?&appid=${apiKey}&q=${location}&units=${units}`,
+    { mode: "cors" }
+  );
+  if (response.status === 400) {
+    const warning = document.getElementById("warning");
+    warning.innerHTML = "Enter a city name";
+    setTimeout(function () {
+      warning.innerHTML = "";
+    }, 2000);
+  } else if (response.status === 404) {
+    const warning = document.getElementById("warning");
+    warning.innerHTML = "Invalid search.";
+    setTimeout(function () {
+      warning.innerHTML = "";
+    }, 2000);
+  } else {
+    const weatherData = await response.json();
+    const cleanData = processData(weatherData);
+    displayData(cleanData);
+  }
 }
 
 function processData(weatherData) {
-    const cleanData = {
+  const cleanData = {
+    desc: weatherData.weather[0].main,
     city: weatherData.name,
-    sky: weatherData.weather[0].main,
     temperature: weatherData.main.temp,
     feels: weatherData.main.feels_like,
     humidity: weatherData.main.humidity,
-    }
+    speed: weatherData.wind.speed,
+  };
 
-    return cleanData
+  return cleanData;
 }
 
 function displayData(cleanData) {
-    const display = document.getElementById('display');
+  const display = document.getElementById("display");
 
-    for (const [key, value] of Object.entries(cleanData)) {
-      const info = (`${key} = ${value}`);
-      const para = document.createElement('p');
-      para.innerText = info;
-      display.appendChild(para);
-    }
+  const desc = document.getElementById("desc");
+  const city = document.getElementById("city");
+  const temp = document.getElementById("temp");
+  const feel = document.getElementById("feel");
+  const humid = document.getElementById("humid");
+  const speed = document.getElementById("speed");
+
+  desc.innerHTML = cleanData.desc;
+  city.innerHTML = cleanData.city;
+  temp.innerHTML = `${cleanData.temperature.toFixed(0)}&#176`;
+  feel.innerHTML = `${cleanData.feels.toFixed(0)}&#176`;
+  humid.innerHTML = `${cleanData.humidity.toFixed(0)}&#37`;
+
+  const speedUnit = unit === "imperial" ? "mph" : "km/h";
+  speed.innerHTML = `${cleanData.speed} ${speedUnit}`;
 }
 
-function clearDisplay() {
-    const display = document.getElementById('display');
-
-    while (display.firstChild) {
-        display.removeChild(display.lastChild);
-      }
+function unitSwitch() {
+  const city = document.getElementById("city");
+  const button = document.getElementById("unitBtn");
+  if (unit === "imperial") {
+    button.innerHTML = "C";
+    button.classList.remove("btn-success");
+    button.classList.add("btn-warning");
+    getWeather(city.innerHTML, "metric");
+    unit = "metric";
+  } else {
+    button.innerHTML = "F";
+    button.classList.remove("btn-warning");
+    button.classList.add("btn-success");
+    getWeather(city.innerHTML, "imperial");
+    unit = "imperial";
+  }
 }
+
+function submitKey(e) {
+  if (e.code === "Enter" || e.code === "NumpadEnter") {
+    getInput();
+  }
+}
+
+getWeather("Miami", unit);
+
+document.addEventListener("keydown", submitKey);
